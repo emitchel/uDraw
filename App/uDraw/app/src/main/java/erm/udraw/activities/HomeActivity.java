2cb26a6711f9f5168ec85de1e123f4f9f9be73cd
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2016-03-06 21:48:56 -0600

    action methods
    added most of the action methods, scrappy code to be cleaned up for
    screen shot (will work on tomorrow)

--- App/uDraw/app/src/main/java/erm/udraw/activities/HomeActivity.java
+++ App/uDraw/app/src/main/java/erm/udraw/activities/HomeActivity.java
@@ -1,25 +1,54 @@
 package erm.udraw.activities;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
 
 import erm.udraw.R;
+import erm.udraw.objects.Utils;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity {
+
+    Context mContext;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public String getTag() {
+        return "Home Activity";
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setUpToolbar();
+        setUpObjects();
+
+    }
+
+    private void setUpObjects(){
+        this.mContext = this;
+    }
+
+    private void setUpToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
     }
 
     @Override
@@ -29,8 +58,82 @@ public class HomeActivity extends AppCompatActivity {
         return true;
     }
 
+
+    private void takeScreenshot() {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            shareScreenshot(imageFile);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM
+            e.printStackTrace();
+        }
+    }
+
+    private void shareScreenshot(File imageFile) {
+        Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+        shareIntent.setType("image/*");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(imageFile));
+        log(imageFile.getAbsolutePath().toString());
+        startActivity(Intent.createChooser(shareIntent, "share"));
+    }
+
+    private void shareImage() {
+        //TODO:Save image, then share the image?
+        //TODO: (option 2) screenshot?
+        takeScreenshot();
+    }
+
+    private void saveFile(){
+
+        final RelativeLayout dialogView = (RelativeLayout)LayoutInflater.from(this).inflate(R.layout.dialog_image_name,null);
+        final EditText fileName = (EditText) dialogView.findViewById(R.id.file_name);
+
+        fileName.setText(Utils.getCurrentDateTime() + ".jpg");
+
+        new AlertDialog.Builder(this)
+                .setView(dialogView)
+                .setTitle(getString(R.string.save_picture))
+                .setMessage(getString(R.string.give_picture_name))
+                .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        if (Utils.isValidString(fileName.getText().toString())) {
+                            //TODO: save image
+                        } else {
+                            Toast.makeText(mContext,getString(R.string.failed_to_specify),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .show();
+    }
+
     private void newImage(){
         new AlertDialog.Builder(this)
+
                 .setTitle(getString(R.string.undo_all_changes))
                 .setMessage(getString(R.string.sure_undo))
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
@@ -55,12 +158,10 @@ public class HomeActivity extends AppCompatActivity {
             newImage();
             return true;
         } else if(id==R.id.save_image){
-            //TODO:
-            //Popup for new name
+            saveFile();
             return true;
         } else if(id==R.id.share_image){
-            //TODO:
-            //Native share popup
+            shareImage();
             return true;
         } else if(id==R.id.import_image){
             //TODO:
--- App/uDraw/app/src/main/java/erm/udraw/objects/Utils.java
+++ App/uDraw/app/src/main/java/erm/udraw/objects/Utils.java
@@ -1,9 +1,23 @@
 package erm.udraw.objects;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by ellio on 3/6/2016.
  *
  * Generic, miscellaneous file contains common methods
  */
 public class Utils {
+
+    public static boolean isValidString(String s){
+        s=s.trim().toLowerCase();
+        return s!=null && !s.equals("") && !s.equals("null") && s.length()!=0;
+    }
+
+    public static String getCurrentDateTime(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String currentDateandTime = sdf.format(new Date());
+        return currentDateandTime;
+    }
 }
